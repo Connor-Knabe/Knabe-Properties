6baaa97e6c33c7ca8348f8d8685d3064996555a7
Connor Knabe <connorknabe@gmail.com>
2013-05-04 12:26:13 -0500

    Edited the featured listings
    Added featured listings coming soon to the row-fluid div

--- index.php
+++ index.php
@@ -6,34 +6,25 @@
   </head>
 
   <body>
-
     <div class="container">
     <?php include("include/navbar.php"); ?>
       <!-- Jumbotron -->
       <div class="jumbotron">
         <h1>Knabe Properties LLC.</h1>
         <p class="lead">My names Kent Knabe and I take great pride in what we do in our business.  We buy up houses that are in need of some TLC or serious repairs. We then have a team of skilled contractors that make them like new again (by putting in all new appliances, granite countertops, carpets, toilets etc.) and then sell them to a new owner that can enjoy the house for years to come knowing it has been done the right way.
-</p>
+        </p>
         <a class="btn btn-large btn-success" href="contact.php">Contact Us!</a>
       </div>
-
       <hr>
-
       <!-- Example row of columns -->
-
       <div class="featured">
         <h1>Featured Listings </h2>
-
       </div>
-
-
-
       <div class="row-fluid">
        <p>
-        No featured listings yet.
+        Featured Listings coming soon
        </p>
       </div>
-
       <hr>
       <?php include("include/footer.php"); ?>
   </body>
