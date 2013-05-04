70893409e4b54160d1c53151d8946cc0ae345ed8
Connor Knabe <Connor-Knabe@users.noreply.github.com>
2013-05-04 12:58:13 -0500

    Cleaned up files

--- logout2.php
+++
@@ -1,5 +0,0 @@
-<?php 
-setcookie('username', '', time()-3600, 'babbage.cs.missouri.edu/~ckgdd/Knabe/'); 
-setcookie('password', '', time()-3600, 'babbage.cs.missouri.edu/~ckgdd/Knabe/');
-header("Location: index.php"); 
-?>
