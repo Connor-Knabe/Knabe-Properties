5ee0ac13035064d4f0e2e4a4159cbd529c6b5e4a
Connor Knabe <connorknabe@gmail.com>
2013-05-06 21:52:36 -0500

    Added chat

---
+++ chat.php
@@ -0,0 +1,36 @@
+<?php include("include/loginheader.php"); ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  <?php include("include/header.php"); ?>
+  </head>
+
+  <body>
+
+    <div class="container">
+     <?php include("include/navbar.php"); ?>
+    </div>
+    <!-- Example row of columns -->
+      <div class="featured">
+        <h2>Welcome to the chat room </h2>
+      </div>
+
+
+		
+
+      <div class="row-fluid">
+        <div id="content" class="span10 offset1">
+          <iframe src="http://peaceful-lake-1188.herokuapp.com/"></iframe>
+        </div>
+		  </div>
+
+      <hr>
+
+
+      <?php include("include/footer.php"); ?>
+
+
+    </div> <!-- /container -->
+
+  </body>
+</html>
