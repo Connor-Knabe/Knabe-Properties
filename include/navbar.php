a88046cf8c9fe202b03497ac183656cc2c1708fe
Connor Knabe <Connor-Knabe@users.noreply.github.com>
2013-05-06 21:52:36 -0500

    Added chat

--- include/navbar.php
+++ include/navbar.php
@@ -8,6 +8,7 @@
             <li <?php if(basename($_SERVER[PHP_SELF])=="renovated.php"){?>class="active"<?php } ?>><a href="renovated.php">Recently Renovated</a></li>
             <?php if(isset($_SESSION["login"])) { ?><li <?php if(basename($_SERVER[PHP_SELF])=="protected.php"){?>class="active"<?php } ?>><a href="protected.php">Protected</a></li><?php } ?>
             <li <?php if(basename($_SERVER[PHP_SELF])=="contact.php"){?>class="active"<?php } ?>><a href="contact.php">Contact</a></li>
+            <li <?php if(basename($_SERVER[PHP_SELF])=="chat.php"){?>class="active"<?php } ?>><a href="chat.php">Chat</a></li>
           </ul>
         </div>
       </div>
